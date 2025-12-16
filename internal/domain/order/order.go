1189172903999a70af1df41912d84f3ec1c5f235
Mord Mora <danielbonnett20@gmail.com>
2025-12-16 16:03:31 -0500

    test commit

--- internal/domain/order/order.go
+++ internal/domain/order/order.go
@@ -1,17 +1,19 @@
 package order
 
 import (
+	"time"
+
 	"github.com/AXONcompany/POS/internal/domain/product"
 	"github.com/AXONcompany/POS/internal/domain/table"
-	"time"
 	"gorm.io/gorm"
 )
 
 type Order struct {
 	gorm.Model
+
 	OrderDate time.Time
 	Total     float64
 	Client    string
 	Products  []*product.Product `gorm:"many2many: order_products;"`
-	Tables 	  []*table.Table  `gorm:"foreignKey:OrderID"`
+	Tables    []*table.Table     `gorm:"foreignKey:OrderID"`
 }
